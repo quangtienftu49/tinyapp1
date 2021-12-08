4dc9b3e1dc1ca74205ffdfaa410b8a379dbcefa1
quangtienftu49 <quangtienftu49@gmail.com>
2021-12-08 14:44:34 +0000

    Added new routes to display database

---
+++ express_server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+// Add routes displaying database
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+// Add new route displaying hello styled
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
